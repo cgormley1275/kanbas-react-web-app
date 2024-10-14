4ab81e82b885394d9a8c48db1c8292a8b63e4443
Chris <cgormley1275@gmail.com>
2024-10-14 14:15:28 +0200

    completed A4 up to 2.3

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -1,3 +1,18 @@
+import ClickEvent from "./ClickEvent";
+import EventObject from "./EventObject";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
+
 export default function Lab1() {
-    <div>Hello World</div>
+    function sayHello() {
+        alert("Hello");
+    }
+    return (
+        <div>
+            <ClickEvent />
+            <PassingDataOnEvent />
+            <PassingFunctions theFunction={sayHello} />
+            <EventObject/>
+        </div>
+    )
 }
