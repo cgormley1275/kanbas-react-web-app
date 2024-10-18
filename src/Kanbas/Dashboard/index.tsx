27c71fcbf889cea3003a63bad03ed82de285c3e1
Chris <cgormley1275@gmail.com>
2024-10-18 20:46:00 +0200

    completed A4 until 3.2

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -8,7 +8,17 @@ export default function Dashboard() {
     startDate: "2023-09-10", endDate: "2023-12-15",
     image: "cs6.jpeg", description: "New Description"
   });
-
+  const updateCourse = () => {
+    setCourses(
+      courses.map((c) => {
+        if (c._id === course._id) {
+          return course;
+        } else {
+          return c;
+        }
+      })
+    );
+  };
   const addNewCourse = () => {
     const newCourse = {
       ...course,
@@ -16,7 +26,9 @@ export default function Dashboard() {
     };
     setCourses([...courses, { ...course, ...newCourse }]);
   };
-
+  const deleteCourse = (courseId: string) => {
+    setCourses(courses.filter((course) => course._id !== courseId));
+  };
   return (
     <div id="wd-dashboard">
       <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
@@ -24,6 +36,10 @@ export default function Dashboard() {
         <button className="btn btn-primary float-end"
           id="wd-add-new-course-click"
           onClick={addNewCourse} > Add </button>
+        <button className="btn btn-warning float-end me-2"
+          onClick={updateCourse} id="wd-update-course-click">
+          Update
+        </button>
       </h5><br />
       <input value={course.name} className="form-control mb-2"
         onChange={(e) => setCourse({ ...course, name: e.target.value })} />
@@ -35,17 +51,34 @@ export default function Dashboard() {
           {courses.map((course) => (
             <div className="wd-dashboard-course col" style={{ width: "300px" }}>
               <div className="card rounded-3 overflow-hidden">
-                <Link to={`/Kanbas/Courses/${course._id}/Home`}
-                  className="wd-dashboard-course-link text-decoration-none text-dark" >
-                  <img src={`/images/${course.image}`} width="100%" height={160} />
-                  <div className="card-body">
-                    <h5 className="wd-dashboard-course-title card-title">
-                      {course.name} </h5>
-                    <p className="wd-dashboard-course-title card-text overflow-y-hidden" style={{ maxHeight: 100 }}>
-                      {course.description} </p>
+
+                <img src={`/images/${course.image}`} width="100%" height={160} />
+                <div className="card-body">
+                  <h5 className="wd-dashboard-course-title card-title">
+                    {course.name} </h5>
+                  <p className="wd-dashboard-course-title card-text overflow-y-hidden" style={{ maxHeight: 100 }}>
+                    {course.description} </p>
+                  <Link to={`/Kanbas/Courses/${course._id}/Home`}
+                    className="wd-dashboard-course-link text-decoration-none text-dark" >
                     <button className="btn btn-primary"> Go </button>
-                  </div>
-                </Link>
+                  </Link>
+                  <button onClick={(event) => {
+                    event.preventDefault();
+                    deleteCourse(course._id);
+                  }} className="btn btn-danger float-end"
+                    id="wd-delete-course-click">
+                    Delete
+                  </button>
+                  <button id="wd-edit-course-click"
+                    onClick={(event) => {
+                      event.preventDefault();
+                      setCourse(course);
+                    }}
+                    className="btn btn-warning me-2 float-end" >
+                    Edit
+                  </button>
+
+                </div>
               </div>
             </div>
           ))}
