abd0987cd6ee31bd29001b3418eb9e2325a048c7
Chris <cgormley1275@gmail.com>
2024-11-10 11:25:04 -0500

    completed A5 up to 4.3.5

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -1,25 +1,46 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import * as client from "./client";
 import { BsGripVertical } from "react-icons/bs";
 import ModulesControls from "./ModulesControls";
 import ModuleControlButtons from "./ModuleControlButtons";
 import LessonControlButtons from "./LessonControlButtons";
 import { useParams } from "react-router";
 import * as db from "../../Database";
-import { addModule, editModule, updateModule, deleteModule }
+import { setModules, addModule, editModule, updateModule, deleteModule }
   from "./reducer";
 import { useSelector, useDispatch } from "react-redux";
 export default function Modules() {
+
   const { cid } = useParams();
   const [moduleName, setModuleName] = useState("");
   const { modules } = useSelector((state: any) => state.modulesReducer);
   const dispatch = useDispatch();
+  const saveModule = async (module: any) => {
+    const status = await client.updateModule(module);
+    dispatch(updateModule(module));
+  };
+  const removeModule = async (moduleId: string) => {
+    await client.deleteModule(moduleId);
+    dispatch(deleteModule(moduleId));
+  };
+  const createModule = async (module: any) => {
+    const newModule = await client.createModule(cid as string, module);
+    dispatch(addModule(newModule));
+  };
   const { currentUser } = useSelector((state: any) => state.accountReducer);
+  const fetchModules = async () => {
+    const modules = await client.findModulesForCourse(cid as string);
+    dispatch(setModules(modules));
+  };
+  useEffect(() => {
+    fetchModules();
+  }, []);
   return (
     <div>
       {currentUser.role === "FACULTY" && <div>
         <ModulesControls moduleName={moduleName} setModuleName={setModuleName}
           addModule={() => {
-            dispatch(addModule({ name: moduleName, course: cid }));
+            createModule({ name: moduleName, course: cid });
             setModuleName("");
           }} /> <br /> <br /> <br />
       </div>}
@@ -33,16 +54,16 @@ export default function Modules() {
                 {!module.editing && module.name}
                 {module.editing && (
                   <input className="form-control w-50 d-inline-block"
-                    onChange={(e) => dispatch(updateModule({ ...module, name: e.target.value }))}
+                    onChange={(e) => saveModule({ ...module, name: e.target.value })}
                     onKeyDown={(e) => {
                       if (e.key === "Enter") {
-                        dispatch(updateModule({ ...module, editing: false }));
+                        saveModule({ ...module, editing: false });
                       }
                     }}
                     defaultValue={module.name} />
                 )}
                 {currentUser.role === "FACULTY" && <ModuleControlButtons moduleId={module._id} deleteModule={(moduleId) => {
-                  dispatch(deleteModule(moduleId));
+                  removeModule(moduleId);
                 }}
                   editModule={(moduleId) => dispatch(editModule(moduleId))} />}
               </div>
