e09b432fc42388a5fe484cbd6facee5093d3babc
Chris <cgormley1275@gmail.com>
2024-11-24 20:10:40 -0500

    completed A6 3.7.1

--- src/Kanbas/Account/Navigation.tsx
+++ src/Kanbas/Account/Navigation.tsx
@@ -1,13 +1,18 @@
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
 export default function AccountNavigation() {
     const { currentUser } = useSelector((state: any) => state.accountReducer);
+    const location = useLocation();
     const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
+    const active = (path: string) => (location.pathname.includes(path) ? "active" : "");
     return (
         <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
-            {!currentUser && <Link to={`/Kanbas/Account/Signin`} className="list-group-item active border border-0"> Signin  </Link> }
-            {!currentUser && <Link to={`/Kanbas/Account/Signup`} className="list-group-item text-danger border border-0"> Signup  </Link> }
-            {currentUser && <Link to={`/Kanbas/Account/Profile`} className="list-group-item text-danger border border-0"> Profile </Link> }
+            {!currentUser && <Link to={`/Kanbas/Account/Signin`} className="list-group-item active border border-0"> Signin  </Link>}
+            {!currentUser && <Link to={`/Kanbas/Account/Signup`} className="list-group-item text-danger border border-0"> Signup  </Link>}
+            {currentUser && <Link to={`/Kanbas/Account/Profile`} className="list-group-item text-danger border border-0"> Profile </Link>}
+            {currentUser && currentUser.role === "ADMIN" && (
+                <Link to={`/Kanbas/Account/Users`} className={`list-group-item ${active("Users")}`}> Users </Link>)}
         </div>
     );
 }
--- src/Kanbas/Account/client.ts
+++ src/Kanbas/Account/client.ts
@@ -24,3 +24,9 @@ export const signout = async () => {
     const response = await axiosWithCredentials.post(`${USERS_API}/signout`);
     return response.data;
 };
+
+export const findAllUsers = async () => {
+    const response = await axiosWithCredentials.get(USERS_API);
+    return response.data;
+};
+
